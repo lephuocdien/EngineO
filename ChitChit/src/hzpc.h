8fdc65c75d19cf966a22c91aa5f48767ed505a35
lephuocdien <ledientesttb@gmail.com>
2021-01-08 18:31:31 +0700

    a

---
+++ ChitChit/src/hzpc.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include<memory>
+#include<utility>
+#include<algorithm>
+#include<functional>
+#include<cstring>
+#ifdef CHITCHIT_PLATFORM_WINDOW
+	#include<Windows.h>
+#endif
